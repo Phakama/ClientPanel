2aa47907e1d980324f158170fe0e5f557bc1886d
Phakama Ntshewula <ntshewulap@gmail.com>
2018-10-11 14:30:09 +0200

    angularfire2 setup & client service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAiBCl1rvHnhYyfSnPE0cXeSfHKzg0norQ",
+    authDomain: "clientpanelprod-f9e3e.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-f9e3e.firebaseio.com",
+    projectId: "clientpanelprod-f9e3e",
+    storageBucket: "clientpanelprod-f9e3e.appspot.com",
+    messagingSenderId: "542230998963"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAiBCl1rvHnhYyfSnPE0cXeSfHKzg0norQ",
+    authDomain: "clientpanelprod-f9e3e.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-f9e3e.firebaseio.com",
+    projectId: "clientpanelprod-f9e3e",
+    storageBucket: "clientpanelprod-f9e3e.appspot.com",
+    messagingSenderId: "542230998963"
+  }
 };
 
 /*
